279086044afae40c876a8ecb2989981c58f07425
DIMPLE SINGHVI <71957186+DIMPLESINGHVI@users.noreply.github.com>
2023-06-24 18:46:29 +0530

    Create reverse_only_letters.cpp

---
+++ reverse_only_letters.cpp
@@ -0,0 +1,32 @@
+#include<cctype>
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int j=s.length()-1,i=0;
+
+        while(i<j)
+        {
+            if(isalpha(s[i]) && isalpha(s[j]))
+            {
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            }
+            else if(!isalpha(s[i])&& isalpha(s[j]))
+            {
+                i++;
+            }
+            else if(isalpha(s[i])&& !isalpha(s[j]))
+            {
+                j--;
+            }
+            else if(!isalpha(s[i])&& !isalpha(s[j]))
+            {
+                i++;
+                j--;
+            }
+
+        }
+        return s;
+    }
+};
